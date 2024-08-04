96a69a419165f5ce5013fc57bf23d10cdfb1be94
zack6yuan <zack6yuan@gmail.com>
2024-08-04 02:56:40 +0000

    Added betty documentation and function prototype.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,3 +2,12 @@
 /**
  * insertion_sort - sorts a foubly linked list of integers 
  * in ascending orderusing the Insertion sort algorithm.
+ * @list: the list that is being sorted
+ * @node1: the first node
+ * @node2: the second node
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+    
+}
