1a3521e1a87e453f96cb7ea2b20bae5c73c49428
zack6yuan <zack6yuan@gmail.com>
2024-08-04 03:06:01 +0000

    Added documentation and function prototype.

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts an array of integers in ascending 
+ * order using the quick sort algorithm
+ * @array: the array that is going to be sorted
+ * @size: the size of the array
+ * Return: void
+*/
+void quick_sort(int *array, size_t size)
+{
+    
+}
