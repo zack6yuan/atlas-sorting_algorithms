7e39bc7df4532f4748ccbb5b4abe9fa1d8d2eb5f
zack6yuan <zack6yuan@gmail.com>
2024-08-04 02:52:12 +0000

    Formatting, betty check will not pass as is.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,12 @@
 #include "sort.h"
 /**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble Sort algorithm
+ * bubble_sort - sorts an array of integers in ascending 
+ * order using the Bubble Sort algorithm
  * @array: the array of integers being sorted
  * @size: the size of the array
  * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-    
+
 }
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,4 @@
+#include "sort.h"
+/**
+ * insertion_sort - sorts a foubly linked list of integers 
+ * in ascending orderusing the Insertion sort algorithm.
