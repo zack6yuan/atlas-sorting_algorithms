ec7409a4374253f247d9072dd70baa496ccb41c3
zack6yuan <zack6yuan@gmail.com>
2024-08-05 19:33:59 +0000

    print array, list, and bubble sort functions

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,5 +8,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+        size_t i; /*loop counter*/
+        size_t o; /*loop counter*/
+        int temp; /*used to swap*/
 
+        if (!array || size < 2) /*if NULL or size < 2*/
+        {
+                return; /*return nothing*/
+        }
+        for (o = 0; o < size - 1; o++)
+        {
+                for (i = 0; i < size - o - 1; i++)
+                {
+                        if (array[i] > array[i + 1)
+                        {
+                                temp = array[i];
+                                array[i] = array[i+1];
+                                array[i + 1] = temp;
+                                print_array(array, size);
+                        }
+                }
+        }
 }
