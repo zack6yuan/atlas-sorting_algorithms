cc748f24e189546c166f5cef2ad5f9d6e57e4f3e
zack6yuan <zack6yuan@gmail.com>
2024-08-05 20:32:28 +0000

    selection sort func completed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,5 +8,31 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    
+    size_t x; /*loop counter*/
+    size_t y; /*loop counter*/
+    size_t min_val;
+    int temp;
+
+    if (!array || size < 2) /*parameter*/
+    {
+        return;
+    }
+    for (x = 0; x < size -1; x++) /*loop*/
+    {
+        min_val = 1
+        for (y = x + 1; y < size; y++) /*loop*/
+        {
+            if (array[y] < array[min_val])
+            {
+                min_val = y;
+            }
+            if (min_val != x)
+            {
+                temp = array[x];
+                array[x] = array[min_val]
+                array[min_val] = temp;
+                print_array(array, size)
+            }
+        }
+    }
 }
