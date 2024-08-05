9d2ba59a7289dc4811c9bfc6a97cedcaf30bd474
zack6yuan <zack6yuan@gmail.com>
2024-08-05 23:41:51 +0000

    forgot to include in main file

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void swap(listint_t **list, listint_t *node_one, listint_t *node_two);
 void print_list(const listint_t *list);
+void recursive_sort(int *array, int start, int end);
+int partition(int *array, int start, int end);
+void swap(int *x, int *y);
 
 #endif /*SORT_H*/
