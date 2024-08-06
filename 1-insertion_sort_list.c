17d775cba92e1ba88d9c36a42ee9ce937a805738
zack6yuan <zack6yuan@gmail.com>
2024-08-06 03:08:48 +0000

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,22 +8,22 @@
  */
 void swap(listint_t **list, listint_t *node_one, listint_t *node_two)
 {
-        if (node_one->prev)
-        {
-                node_one->prev->next = node_two;
-        }
-        else
-        {
-                *list = node_two;
-        }
-        if (node_two->next)
-        {
-                node_two->next->prev = node_one;
-        }
-        node_one->next = node_two->next;
-        node_two->prev = node_one ->prev;
-        node_one->prev = node_two;
-        node_two->next = node_one;
+if (node_one->prev)
+{
+node_one->prev->next = node_two;
+}
+else
+{
+*list = node_two;
+}
+if (node_two->next)
+{
+node_two->next->prev = node_one;
+}
+node_one->next = node_two->next;
+node_two->prev = node_one ->prev;
+node_one->prev = node_two;
+node_two->next = node_one;
 }
 /**
  * insertion_sort - sorts a doubly linked list of integers 
@@ -35,22 +35,22 @@ void swap(listint_t **list, listint_t *node_one, listint_t *node_two)
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *current;
-        listint_t *next_node;
+listint_t *current;
+listint_t *next_node;
 
-        if (!list || !*list || !(*list))
-        {
-                return;
-        }
-        current = (*list)->next;
-        while (current)
-        {
-                next_node = current->next;
-                while (current->prev && current->prev->n > current->n)
-                {
-                        swap(list, current->prev, current);
-                        print_list(*list);
-                }
-                current = next_node;
-        }
+if (!list || !*list || !(*list))
+{
+return;
+}
+current = (*list)->next;
+while (current)
+{
+next_node = current->next;
+while (current->prev && current->prev->n > current->n)
+{
+swap(list, current->prev, current);
+print_list(*list);
+}
+current = next_node;
+}
 }
