a628dd7ded5d6d9ac5ae2f70c64b0d8c4b48e973
zack6yuan <zack6yuan@gmail.com>
2024-08-14 08:25:20 +0000

    changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,33 @@
 #include "sort.h"
 /**
+ * insertion_sort _list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: the list that is being sorted
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *current;
+listint_t *next_node;
+
+if (!list || !*list || !(*list))
+{
+return;
+}
+current = (*list)->next;
+while (current)
+{
+next_node = current->next;
+while (current->prev && current->prev->n > current->n)
+{
+swap(list, current->prev, current);
+print_list(*list);
+}
+current = next_node;
+}
+}
+
+/**
  * swap - swaps two nodes in a list
  * @node_one: the first node
  * @node_two: the second node
@@ -24,31 +52,4 @@ node_one->next = node_two->next;
 node_two->prev = node_one->prev;
 node_one->prev = node_two;
 node_two->next = node_one;
-}
-/**
- * insertion_sort _list - sorts a doubly linked list of integers
- * in ascending orderusing the Insertion sort algorithm.
- * @list: the list that is being sorted
- * Return: void
- */
-void insertion_sort_list(listint_t **list)
-{
-listint_t *current;
-listint_t *next_node;
-
-if (!list || !*list || !(*list))
-{
-return;
-}
-current = (*list)->next;
-while (current)
-{
-next_node = current->next;
-while (current->prev && current->prev->n > current->n)
-{
-swap(list, current->prev, current);
-print_list(*list);
-}
-current = next_node;
-}
 }
