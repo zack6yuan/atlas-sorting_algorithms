f7f849257fde2c86caad43a6c50be35b771c3dae
zack6yuan <zack6yuan@gmail.com>
2024-08-14 08:23:20 +0000

    changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -26,11 +26,9 @@ node_one->prev = node_two;
 node_two->next = node_one;
 }
 /**
- * insertion_sort - sorts a doubly linked list of integers
+ * insertion_sort _list- sorts a doubly linked list of integers
  * in ascending orderusing the Insertion sort algorithm.
  * @list: the list that is being sorted
- * @node1: the first node
- * @node2: the second node
  * Return: void
  */
 void insertion_sort_list(listint_t **list)
